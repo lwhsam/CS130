896f3fbd1926a77f078f634af6bf21d0ec7865c6
lwhsam <samliwahei@gmail.com>
2022-05-26 18:08:44 -0500

    Homework4

--- Homework/Homework04/your_task/js/scripts.js
+++ Homework/Homework04/your_task/js/scripts.js
@@ -28,19 +28,24 @@ const getTracks = (term) => {
     .then((data) => {
         console.log("tracks:", data);
         const firstFive = data.slice(0,5);
-        for (const artistData of firstFive) {
-            console.group(artistData);
-            elem.innerHTML += getTrackHTML(artistData);
+        if(data.length>0){
+            for (const artistData of firstFive) {
+                console.group(artistData);
+                elem.innerHTML += getTrackHTML(artistData);
+            }
+        }
+        else {
+            elem.innerHTML = "track not found";
         }
     });
 };
 
 const getTrackHTML = (data) => {
-    return `<button class="track-item preview" data-preview-track=${data.preview_url}>
-    <img src=${data.album.image_url}>
+    return `<button class="track-item preview" data-preview-track=${data.preview_url}" photo=${data.album.image_url} song-name=${data.name} artist-name=${data.artist.name} title='Play track - ${data.name}' onclick='handleTrackClick(event)'>
+    <img src=${data.album.image_url} , alt="Photo of ${data.name}">
     <i class="fas play-track fa-play" aria-hidden="true"></i>
     <div class="label">
-        <h2>${data.album.name}</h2>
+        <h2>${data.name}</h2>
         <p>
             ${data.artist.name}
         </p>
@@ -55,6 +60,35 @@ const getAlbums = (term) => {
         get albums from spotify based on the search term
         "${term}" and load them into the #albums section 
         of the DOM...`);
+        const elem = document.querySelector("#albums");
+        elem.innerHTML = '';
+        fetch(baseURL + "?type=album&q=" + term)
+        .then((data) => data.json())
+        .then((data) => {
+        if(data.length>0){
+            for (const albumData of data) {
+                console.log("album:", albumData);
+                elem.innerHTML += getAlbumHTML(albumData);
+            }
+        }
+        else {
+            elem.innerHTML = "album not found";
+        }
+    });    
+};
+
+const getAlbumHTML = (data) => {
+    return `<section class="album-card" id=${data.id}>
+    <div>
+        <img src=${data.image_url} , alt="Photo of ${data.name}">
+        <h2>${data.name}</h2>
+        <div class="footer">
+            <a href=${data.spotify_url} target="_blank" title="View ${data.name} on Spotify">
+                view on spotify
+            </a>
+        </div>
+    </div>
+    </section>`
 };
 
 const getArtist = (term) => {
@@ -82,10 +116,10 @@ const getArtist = (term) => {
 const getArtistHTML = (data) => {
     return `<section class="artist-card" id=${data.id}>
     <div>
-        <img src=${data.image_url}>
+        <img src=${data.image_url} , alt="Photo of ${data.name}">
         <h2>${data.name}</h2>
         <div class="footer">
-            <a href=${data.spotify_url}>
+            <a href=${data.spotify_url} title="View ${data.name} on Spotify">
                 view on spotify
             </a>
         </div>
@@ -94,8 +128,24 @@ const getArtistHTML = (data) => {
 }
 
 const handleTrackClick = (ev) => {
+    const photo = ev.currentTarget.getAttribute('photo');
+    const songName = ev.currentTarget.getAttribute('song-name');
+    const artistName = ev.currentTarget.getAttribute('artist-name');
     const previewUrl = ev.currentTarget.getAttribute('data-preview-track');
-    console.log(previewUrl);
+    console.log(previewUrl); 
+    document.querySelector('#track').src = previewUrl
+    const audio = document.querySelector('#track');
+    audio.load();
+    audio.play();
+    document.querySelector('#current-track').innerHTML =
+    `<img src=${photo}>
+    <i class="fas play-track fa-pause" aria-hidden="true"></i>
+    <div class="label">
+        <h2>${songName}</h2>
+        <p>
+            ${artistName}
+        </p>
+    <div/>`
 }
 
 document.querySelector('#search').onkeyup = (ev) => {
